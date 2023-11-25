8e529ed9a7635ffe4530f6603d66abd77bea9c8b
JoseVictorLDC <josevictorldc@gmail.com>
2023-11-25 17:20:58 -0300

    Acertando detalhes do perfil

--- frontend/src/pages/PerfilPage.jsx
+++ frontend/src/pages/PerfilPage.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
 import {Link } from "react-router-dom";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 import "./PerfilPage.css"
 
 const PerfilPage  = ({nome, email, telefone, senha}) => {
-
     return (
         <>
-            <div class="fundo">
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <div class="opcoes">
-                <Link to="/dadosPessoais">
-                <button className="ButtonDados">Dados pessoais</button>
-                </Link>
-                <div className="walletButton">
-                    <ConnectButton />
-                </div>
-            </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            </div>
-            <Footer></Footer>
+        <div class="fundo">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div class="opcoes">
+            <Link to="/perfil">
+            <button class="ButtonDados">Dados pessoais</button>
+            </Link>
+            <Link to="/carteiraVirtual">
+            <button class="ButtonDados">Carteira virtual</button>
+            </Link>
+        </div>
+        <div class="dados-pessoais">
+            <h1>Olá {nome}!!!</h1>
+            <h1>Nome:</h1>
+            <h1>{nome}</h1>
+            <h1>Email:</h1>
+            <h1>{email}</h1>
+            <h1>Telefone:</h1>
+            <h1>{telefone}</h1>   
+            <h1>Senha:</h1>
+            <h1>{senha}</h1>
+        </div>
+        </div>
+        <Footer></Footer>
         </>
     );
   }
