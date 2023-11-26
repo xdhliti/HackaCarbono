fb2eb553ea8579f50bde79739a054ab46619a87f
xdhliti <guilherme.jacule@hotmail.com>
2023-11-26 00:08:19 -0300

    pronto pra deploy acho

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,23 +5,23 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   
   define: {
-    global: 'globalThis',
+    //global: 'globalThis',
     // CHANGE BETWEEN THIS TO DECLARATIONS. BELOW IS FOR BUILD/DEPLOY
-    // global: (() => {
-    //   let globalVariable = 'globalThis';
-    //   try {
-    //     // Try to import @safe-global/safe-apps-provider
-    //     require.resolve('@safe-global/safe-apps-provider');
-    //     // Try to import @safe-global/safe-apps-sdk
-    //     require.resolve('@safe-global/safe-apps-sdk');
-    //     // If both modules are found, return the custom global variable
-    //     globalVariable = 'global';
-    //    } catch (e) {
-    //     // If either module is not found, fallback to globalThis
-    //     globalVariable = 'globalThis';
-    //    }
-    //   return globalVariable;
-    //  })()
+    global: (() => {
+       let globalVariable = 'globalThis';
+       try {
+         // Try to import @safe-global/safe-apps-provider
+         require.resolve('@safe-global/safe-apps-provider');
+         // Try to import @safe-global/safe-apps-sdk
+         require.resolve('@safe-global/safe-apps-sdk');
+         // If both modules are found, return the custom global variable
+         globalVariable = 'global';
+        } catch (e) {
+         // If either module is not found, fallback to globalThis
+         globalVariable = 'globalThis';
+        }
+       return globalVariable;
+      })()
   },
   resolve: {
     alias: {
